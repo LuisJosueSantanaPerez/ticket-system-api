f2899d2439781d353457b701ad1ccf1b2e420815
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-12 23:56:16 -0400

    adding seeder the user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         \App\Models\Departments::factory(10)->create();
         \App\Models\Employees::factory(15)->create();
     }
