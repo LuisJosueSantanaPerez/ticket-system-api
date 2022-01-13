c83196329debf553f5c7a4b7b131128e1fbcbc13
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-13 11:23:24 -0400

    fake data employee factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -4,17 +4,17 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use App\Models\Departments;
-use App\Models\Employees;
+use App\Models\Department;
+use App\Models\Employee;
 
-class EmployeesFactory extends Factory
+class EmployeeFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Employees::class;
+    protected $model = Employee::class;
 
     /**
      * Define the model's default state.
@@ -27,8 +27,8 @@ class EmployeesFactory extends Factory
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->safeEmail,
-            'department_id' => Departments::factory(),
-            'is_active' => $this->faker->boolean,
+            'department_id' => Department::factory(),
+            'activated' => $this->faker->boolean,
         ];
     }
 }
