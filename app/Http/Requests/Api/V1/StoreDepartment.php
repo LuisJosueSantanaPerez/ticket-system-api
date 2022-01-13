8cb7e3e5594d9b5f91a4af587ae4335a85009060
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-13 11:22:08 -0400

    StoreDepartment validation input request

---
+++ app/Http/Requests/Api/V1/StoreDepartment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreDepartment extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description' => 'required|string|max:50',
+            'activated' => 'boolean'
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors()->all(), 422));
+    }
+
+}
