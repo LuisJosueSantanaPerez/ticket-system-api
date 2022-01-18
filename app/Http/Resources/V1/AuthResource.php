d77a693e92fb15135e8b0881ddce4a622ca513a4
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-17 23:56:29 -0400

    create AuthResource

---
+++ app/Http/Resources/V1/AuthResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'success' => true,
+            'message' => 'Success',
+            'data' => [
+                'id' => $this->id,
+                'first_name' => $this->first_name,
+                'last_name' => $this->last_name,
+                'email' => $this->email,
+                'token' => $request->user()->createToken($request->device)->plainTextToken,
+            ],
+            'meta' => [
+                'organization' => 'NTI'
+            ],
+            'type'=> 'login',
+        ];
+    }
+}
