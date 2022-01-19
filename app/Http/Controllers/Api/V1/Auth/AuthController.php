1fa6fcce0faccb41a566e5ad83bfcfb9ba35cc79
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-19 00:32:03 -0400

    add new method verify

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreAuthRequest;
 use App\Http\Resources\V1\DepartmentCollection;
 use App\Http\Resources\V1\AuthResource;
+use App\Http\Resources\V1\EmployeeResource;
 use App\Models\Employee;
 use App\Traits\RespondsWithHttpStatus;
 use Illuminate\Contracts\Foundation\Application;
@@ -36,5 +37,20 @@ class AuthController extends Controller
     {
         auth()->user()->tokens()->delete();
         return $this->success('Successfully logout' , null,"logout");
+    }
+
+    public function verify(Request $request){
+        try {
+            $user = [
+                'id' => $request->user()->number,
+                'first_name'=> $request->user()->first_name,
+                'last_name'=> $request->user()->last_name,
+                'email'=> $request->user()->email,
+                'token' => $request->bearerToken()
+            ];
+            return $this->success('Successfully verify' , $user ,"verify");
+        } catch (\Exception $e){
+            return $this->failure('Unauthorized', "verify",401);
+        }
     }
 }
