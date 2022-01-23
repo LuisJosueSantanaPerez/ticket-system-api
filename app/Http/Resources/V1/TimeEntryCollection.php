30adba568804fe912eabe4d31c92dfe61c0907f2
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-23 18:16:45 -0400

    create collection TimeEntryCollection.php

--- app/Http/Resources/V1/TimeEntryCollection.php
+++ app/Http/Resources/V1/TimeEntryCollection.php
@@ -2,18 +2,42 @@
 
 namespace App\Http\Resources\V1;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use JsonSerializable;
 
 class TimeEntryCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'success' => true,
+            'message' => 'Success',
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'NTI'
+            ],
+            'type'=> 'time-entry',
+        ];
+    }
+
+    public function withResponse($request, $response)
+    {
+        $jsonResponse = json_decode($response->getContent(), true);
+        unset(
+            $jsonResponse['links'],
+            $jsonResponse['meta']['links'],
+            $jsonResponse['meta']['from'],
+            $jsonResponse['meta']['to'],
+            $jsonResponse['meta']['path'],
+        );
+        $response->setContent(json_encode($jsonResponse));
     }
 }
