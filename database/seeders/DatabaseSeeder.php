b19e185ab47c5169afa5954b7cffa3f3567455a1
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-23 17:27:28 -0400

    automatic data loading with laravel seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +17,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Employee::factory(4)->create();
-        \App\Models\Category::factory(4)->create();
-        \App\Models\Kind::factory(4)->create();
-        \App\Models\Priority::factory(4)->create();
-        \App\Models\Status::factory(4)->create();
+
+        DB::table('statuses')->insert([
+            ['name' => 'Open'], ['name' => 'Process'], ['name' => 'Closed']
+        ]);
+        DB::table('categories')->insert([
+            ['name' => 'Support'], ['name' => 'IT Department'],
+            ['name' => 'Programmer'], ['name' => 'Marketing'],
+            ['name'=> 'Data base']
+        ]);
+        DB::table('kinds')->insert([
+            ['name' => 'Ticket'], ['name' => 'Bug'], ['name' => 'Suggestion'],
+            ['name' => 'Feature']
+        ]);
+
+        DB::table('priorities')->insert([
+            ['name' => 'High'], ['name' => 'Medium'], ['name' => 'low'],
+        ]);
+
+        Employee::create(
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('admin1234'),
+                'email_verified_at' => now()
+            ]
+        );
+        //\App\Models\Employee::factory(4)->create();
+        // \App\Models\Category::factory(4)->create();
+        // \App\Models\Kind::factory(4)->create();
+        // \App\Models\Priority::factory(4)->create();
+        // \App\Models\Status::factory(4)->create();
     }
 }
