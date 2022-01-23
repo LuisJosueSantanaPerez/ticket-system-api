8e86557756f63da5d62c17cfbe653124c43e535a
Luis Josue Santana Perez <josuesantana17@gmail.com>
2022-01-23 17:27:56 -0400

    add new route

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\AuthController as AuthV1;
 use App\Http\Controllers\Api\V1\Ticket\TicketController as TicketV1;
 use App\Http\Controllers\Api\V1\Ticket\TicketEmployeeController as TicketEmployeeV1;
+use App\Http\Controllers\Api\V1\Ticket\TicketTimeEntryController as TicketTimeEntryV1;
 use App\Http\Controllers\Api\V1\Employee\EmployeeController as EmployeeV1;
 use App\Http\Controllers\Api\V1\Employee\EmployeeTicketControlleer as EmployeeTicketV1;
 use App\Http\Controllers\Api\V1\TimeEntry\TimeEntryController as TimeEntryV1;
+use App\Http\Controllers\Api\V1\Report\ReportController as ReportV1;
 use App\Http\Controllers\Api\V1\Priority\PriorityController as PriorityV1;
 use App\Http\Controllers\Api\V1\Status\StatusController as StatusV1;
 use App\Http\Controllers\Api\V1\Category\CategoryController as CategoryV1;
@@ -49,6 +51,12 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function (){
     // tickets
     Route::apiResource('tickets', TicketV1::class)
         ->only(['index','store', 'show', 'update', 'destroy']);
+    //ticket/{id}/time-entry
+    Route::apiResource('tickets/{ticket}/time-entries', TicketTimeEntryV1::class)
+        ->only(['index', 'show']);
+    // reports
+    Route::apiResource('reports', ReportV1::class)
+        ->only(['index']);
     // assigned-tickets-employees
     Route::apiResource("assigned-tickets-employees", TicketEmployeeV1::class)
         ->only(['index', 'show']);
